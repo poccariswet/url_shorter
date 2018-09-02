5de8a90721d77f578a5651319b3ba99b1fa7d0f4
soeyusuke <s1240231@gmail.com>
2018-09-02 22:03:10 +0900

    [ADD] storage Close

--- storage/mysql/mysql.go
+++ storage/mysql/mysql.go
@@ -1,8 +1,20 @@
 package mysql
 
-import "github.com/soeyusuke/gitclone/ursho/storage"
+import (
+	"database/sql"
+
+	"github.com/soeyusuke/gitclone/ursho/storage"
+)
+
+type mysql struct {
+	DB *sql.DB
+}
 
 func New() (storage.Service, error) {
 
 	return storage.Service{}, nil
+}
+
+func (m *mysql) Close() error {
+	return m.DB.Close()
 }
