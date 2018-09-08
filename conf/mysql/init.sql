4c19aade3ab9432b9d3bedc986cac6ac24e0e703
soeyusuke <s1240231@gmail.com>
2018-09-08 20:42:02 +0900

    [ADD] table create

--- conf/mysql/init.sql
+++ conf/mysql/init.sql
@@ -1,3 +1,11 @@
 use shorten_db;
 
-CREATE TABLE `` ()
+CREATE TABLE `urlshorten` (
+  id INTEGER(11) NOT NULL AUTO_INCREMENT,
+  long_url TEXT NOT NULL,
+  short_url VARCHAR(255) NOT NULL,
+  count INTEGER(11) DEFAULT 0,
+  PRIMARY KEY (id),
+  PRIMARY KEY (long_url),
+  PRIMARY KEY (short_url),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
