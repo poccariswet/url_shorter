de38253205fc80a5d483a23050800e74047021ea
soeyusuke <s1240231@gmail.com>
2018-09-23 15:54:44 +0900

    [FIX] del

--- storage/storage.go
+++ storage/storage.go
@@ -1,12 +1,5 @@
 package storage
 
-type Service interface {
-	Save(string) error
-	CountUrl(string) error
-	Load() (*Urlsho, error)
-	Close() error
-}
-
 type Urlsho struct {
 	Id      int    `json:"id"`
 	LongURL string `json:"long_url"`
