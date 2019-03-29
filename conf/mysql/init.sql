42c9e7305df3472b39d97242087b7cdf29650de1
poccariswet <s1240231@gmail.com>
2019-03-29 17:46:01 +0900

    [FIX]

--- conf/mysql/init.sql
+++
@@ -1,9 +0,0 @@
-use urlsho_db;
-
-CREATE TABLE `urlsho` (
-  id INTEGER(11) NOT NULL AUTO_INCREMENT,
-  long_url VARCHAR(255) NOT NULL,
-  count INTEGER(11) DEFAULT 0,
-  PRIMARY KEY (id),
-  UNIQUE KEY (long_url)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ hash/hash.go
@@ -0,0 +1,6 @@
+package hash
+
+//UUIDで生成された最初の8文字を返す
+func Hash() (string, error) {
+
+}
